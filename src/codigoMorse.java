8834715eb6dc2209a94b9c41053261912e0d85c9
Raul Varandela <raulvarandela@protonmail.com>
2020-06-26 15:55:20 +0200

    Initial commit

---
+++ src/codigoMorse.java
@@ -0,0 +1,33 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class codigoMorse {
+    public static void main(String[] args) {
+        int respuesta = 1;
+        try {
+            while (respuesta != 0) {
+                Diccionario diccionario = new Diccionario();
+                StringBuilder salida = new StringBuilder();
+                System.out.print("Introduce tu frase a pasar a código morse: ");
+                Scanner sc = new Scanner(System.in);
+                String frase = sc.nextLine();
+                frase = frase.toLowerCase();
+
+                for (int i = 0; i < frase.length(); i++) {
+                    salida.append(diccionario.get(frase.charAt(i))).append(" ");
+                }
+                System.out.println("La traducción a morse es: ");
+                System.out.println(salida);
+                do {
+                    System.out.print("Quieres volver a tirar los dados? 1 --> si / 0 --> no: ");
+                    sc = new Scanner(System.in);
+                    respuesta = sc.nextInt();
+                } while (respuesta != 0 && respuesta != 1);
+            }
+        } catch (
+                InputMismatchException ex) {
+            System.out.println("Error: Se ha introducido un caracter o cadena no aceptados :(");
+        }
+
+    }
+}
